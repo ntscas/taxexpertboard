644ca2fa9407f735dffba36cd049a3c49e36616e
ntscas <ntscas@gmail.com>
2026-06-03 23:27:05 +0900

    Add files via upload

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 // Body parser
 app.use(express.json());
